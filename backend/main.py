87aedeae01b6244fb23348283ea12bd9e0c7fa7f
bdegley4789 <egleyb@oregonstate.edu>
2019-09-17 22:09:58 -0700

    set up choice endpoint

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,13 @@ import random
 import json
 
 app = Flask(__name__)
+options = [
+    {"id":1,"name":"rock"},
+    {"id":2,"name":"paper"},
+    {"id":3,"name":"scissor"},
+    {"id":4,"name":"lizard"},
+    {"id":5,"name":"spock"}
+    ]
 
 # I got the basic set up for this flask app here: https://pythonspot.com/flask-web-app-with-python/
 @app.route("/")
@@ -17,13 +24,11 @@ def random_number():
 
 @app.route("/choices")
 def choices():
-    return json.dumps([
-        {"id":1,"name":"rock"},
-        {"id":2,"name":"paper"},
-        {"id":3,"name":"scissor"},
-        {"id":4,"name":"lizard"},
-        {"id":5,"name":"spock"}
-        ])
+    return json.dumps(options)
+
+@app.route("/choice")
+def choice():
+    return options[random.randint(0,len(options)-1)]
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
